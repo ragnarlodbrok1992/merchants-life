0775455af04be551ee99ab6d4fe8245d22ff3901
ragnarlodbrok1992 <maraus92@gmail.com>
2023-07-26 17:22:50 +0200

    Initial commit

---
+++ build.bat
@@ -0,0 +1,21 @@
+@echo off
+SET compiler_dir="C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Tools\MSVC\14.31.31103\bin\Hostx64\x64\cl.exe"
+
+SET header_file_glfw3="D:\libraries\glfw-3.3.8\include"
+SET header_file_glad="D:\libraries\glad\include"
+SET lib_file_glfw="D:\libraries\glfw-3.3.8\build\src\Debug\glfw3.lib"
+SET glad_main_file="D:\libraries\glad\src\glad.c"
+
+REM /Wall - disabling for now @TODO: make warning sensible
+REM /link /LIBPATH:%lib_file_glfw%^
+REM /link /verbose:lib^
+
+%compiler_dir% /EHsc /Zi /MDd^
+ /DEBUG:FULL^
+ /INCREMENTAL:NO^
+ /Fe:"merchants-life"^
+ %glad_main_file%^
+ src/main.cpp^
+ /I%header_file_glfw3% /I%header_file_glad%^
+ OpenGL32.lib User32.lib gdi32.lib Shell32.lib %lib_file_glfw%^
+
---
+++ src/main.cpp
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <glad\glad.h>
+#include <GLFW\glfw3.h>
+
+// Functions forward declarations
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+// Engine constants
+const unsigned int SCREEN_WIDTH = 1024;
+const unsigned int SCREEN_HEIGHT = 768;
+
+int main(int argc, char* argv[]) {
+  printf("Merchants Life - development version.\n");
+
+  // GLFW - initialize and configure
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  // GLFW - window creation
+  GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Merchants Life - developer version.", NULL, NULL); 
+  if (window == NULL) {
+    printf("Failed to create GLFW window!\n");
+    glfwTerminate();
+
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  // GLAD - loading all OpenGL function pointers
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    printf("Failed to initialize GLAD!\n");
+
+    return -1;
+  }
+
+  // Render loop
+
+  while (!glfwWindowShouldClose(window)) {
+    // Processing input at start of every frame
+    processInput(window);
+
+    // Render
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // GLFW - swap buffers and poll IO events
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  // Terminating glfw
+  glfwTerminate();
+
+  // Last call before dropping
+  // If this message wasn't printed - something is terribly wrong
+  printf("Successfully exiting!\n");
+  return 0;
+}
+
+// Processing input
+void processInput(GLFWwindow* window) {
+
+}
+
+// Resizing window
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+  // Viewport should match window size
+  glViewport(0, 0, width, height);
+}
+
